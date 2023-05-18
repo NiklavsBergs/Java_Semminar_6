2394381e11d71c1272a4af4106e0df9ab8521fe7
unknown <localuser@vea.lv>
2023-05-18 13:19:51 +0300

    first commit

---
+++ src/main/java/lv/venta/JavaSemminar6Application.java
@@ -0,0 +1,13 @@
+package lv.venta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSemminar6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSemminar6Application.class, args);
+	}
+
+}
