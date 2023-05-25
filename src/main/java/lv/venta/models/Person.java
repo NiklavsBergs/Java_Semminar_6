5f83f0912db989c393dcf1f5bf91ecd8fbf21cf8
localuser <localuser@VEA.LV>
2023-05-25 16:33:17 +0300

    class person

---
+++ src/main/java/lv/venta/models/Person.java
@@ -0,0 +1,47 @@
+package lv.venta.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@MappedSuperclass
+public class Person {
+	@Column(name = "Idp")
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Setter(value = AccessLevel.NONE)
+	private long idp;
+	
+	@Column(name = "Name")
+	@Size(min = 3, max = 30)
+	@Pattern(regexp = "[A-Z]{1}[a-z\\ ]+", message = "Only latin letters")
+	@NotNull
+	private String name;
+	
+	@Column(name = "Surname")
+	@Size(min = 3, max = 30)
+	@Pattern(regexp = "[A-Z]{1}[a-z\\ ]+", message = "Only latin letters")
+	@NotNull
+	private String surname;
+	
+	public Person(
+			@Size(min = 3, max = 30) @Pattern(regexp = "[A-Z]{1}[a-z\\ ]+", message = "Only latin letters") @NotNull String name,
+			@Size(min = 3, max = 30) @Pattern(regexp = "[A-Z]{1}[a-z\\ ]+", message = "Only latin letters") @NotNull String surname) {
+		this.name = name;
+		this.surname = surname;
+	}
+}
